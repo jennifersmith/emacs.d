54cc786ae0823dacf66391d60a813a273228763d
Jennifer Smith <jennifer@home>
2013-07-31 11:10:14 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,34 @@
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(markdown-command "/usr/local/bin/markdown")
+ '(nrepl-popup-stacktraces-in-repl t)
+ '(tool-bar-mode nil)
+ '(whitespace-style (quote (face tabs trailing lines space-before-tab empty space-after-tab tab-mark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'whitespace-mode)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t) 
