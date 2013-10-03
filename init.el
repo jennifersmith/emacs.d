f8d8b0d38279c16814dd652a8accfcd5455efa67
Jennifer Smith <jennifer@home>
2013-10-03 11:53:16 +0100

    Backed up

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
  '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
  '(markdown-command "/usr/local/bin/markdown")
+ '(nrepl-history-file "~/.nreplhistory")
  '(nrepl-popup-on-error t)
  '(nrepl-popup-stacktraces t)
  '(nrepl-popup-stacktraces-in-repl t)
